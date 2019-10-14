2c0eeceb47f1b8e59e665fb76664480c3e839bb4
mt1sk <mt1sk@mail.ru>
2019-10-14 16:26:31 +0300

    access token after registration.

--- src/app/Http/Controllers/Api/Auth/RegisterController.php
+++ src/app/Http/Controllers/Api/Auth/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\StoreUserRequest;
 use App\User;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
@@ -17,6 +18,19 @@ class RegisterController extends Controller
         $user->password = Hash::make($request->password);
         $user->save();
 
-        return response(null, 201);
+        $token = $user->createToken(config('app.name').', auto api auth.');
+        $token->token->expires_at = Carbon::now()->addMonth();
+        $token->token->save();
+
+        $tokenResult = [
+            'token_type' => 'Bearer',
+            'access_token' =>  $token->accessToken,
+            'expires_at' => Carbon::parse($token->token->expires_at)->toDateTimeString(),
+        ];
+        return response()->json([
+            'data' => [
+                'token'=>$tokenResult,
+            ],
+        ], 201);
     }
 }
